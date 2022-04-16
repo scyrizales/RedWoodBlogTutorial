d1a876199618de29fb18e5662d0b2a5fb379a5a9
Sergio Castillo <scyrizales@gmail.com>
2022-04-16 15:52:21 -0700

    Authentication

--- web/src/layouts/PostsLayout/PostsLayout.js
+++ web/src/layouts/PostsLayout/PostsLayout.js
@@ -1,25 +1,22 @@
 import { Link, routes } from '@redwoodjs/router'
 import { Toaster } from '@redwoodjs/web/toast'
-import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
 
 const PostsLayout = ({ children }) => {
   return (
-    <BlogLayout>
-      <div className="rw-scaffold">
-        <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
-        <header className="rw-header">
-          <h1 className="rw-heading rw-heading-primary">
-            <Link to={routes.posts()} className="rw-link">
-              Posts
-            </Link>
-          </h1>
-          <Link to={routes.newPost()} className="rw-button rw-button-green">
-            <div className="rw-button-icon">+</div> New Post
+    <div className="rw-scaffold">
+      <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
+      <header className="rw-header">
+        <h1 className="rw-heading rw-heading-primary">
+          <Link to={routes.posts()} className="rw-link">
+            Posts
           </Link>
-        </header>
-        <main className="rw-main">{children}</main>
-      </div>
-    </BlogLayout>
+        </h1>
+        <Link to={routes.newPost()} className="rw-button rw-button-green">
+          <div className="rw-button-icon">+</div> New Post
+        </Link>
+      </header>
+      <main className="rw-main">{children}</main>
+    </div>
   )
 }
 
